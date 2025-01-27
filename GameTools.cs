d6ad40337b8e90cf4126299bb5b03235aa81fa90
trey1144 <trey1144@hotmail.com>
2025-01-27 09:57:55 -0700

    Updated code to submit :)

---
+++ GameTools.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace Project4
+{
+    class GameTools
+    {
+        // Method to print the board
+        public void PrintBoard(string[] board)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine($" {board[0]} | {board[1]} | {board[2]} ");
+            Console.WriteLine("---|---|---");
+            Console.WriteLine($" {board[3]} | {board[4]} | {board[5]} ");
+            Console.WriteLine("---|---|---");
+            Console.WriteLine($" {board[6]} | {board[7]} | {board[8]} ");
+            Console.WriteLine("\n");
+        }
+        // Method to check if there is a winner
+        public bool CheckWinner(string[] board, string player)
+        {
+            // setting up the arrays with the rows and columns
+            int[][] winningCombinations = new int[][]
+            {
+                new int[] { 0, 1, 2 }, // Top row
+                new int[] { 3, 4, 5 }, // Middle row
+                new int[] { 6, 7, 8 }, // Bottom row
+                new int[] { 0, 3, 6 }, // Left column
+                new int[] { 1, 4, 7 }, // Middle column
+                new int[] { 2, 5, 8 }, // Right column
+                new int[] { 0, 4, 8 }, // Diagonal top-left to bottom-right
+                new int[] { 2, 4, 6 } // Diagonal top-right to bottom-left
+            };
+            // checking to see if each player won
+            foreach (var combination in winningCombinations)
+            {
+                // if there are three in a row
+                if (board[combination[0]] == player &&
+                    board[combination[1]] == player &&
+                    board[combination[2]] == player)
+                {
+                    return true;
+                }
+            }
+            // no win is met
+            return false;
+        }
+        // checking if the board is full for a tie
+        public bool IsBoardFull(string[] board)
+        {
+            foreach (var spot in board)
+            {
+                // seeing if the board is full
+                if (spot != "X" && spot != "O")
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,79 +1,62 @@
-﻿// Welcome the user
-Console.WriteLine("Welcome to Tic-Tac-Toe!");
+﻿// 2-13 Group: Summer Sampson, Kate Sargent, Ainsley Rossiter, Trey Hammond
+using System;
 
-using Project4;
-
-gameTools gt = new gameTools();
-
-bool gameOver = false;
-
-char[] board = new char[9];
-
-string input = "";
-
-int player = 0;
-
-while (gameOver == false) 
+namespace Project4
 {
-    // Display the board
-    gt.displayBoard(board);
-
-    Console.WriteLine("Player X, please enter the number of an open space between 1 and 9: ");
-
-    while (true)
+    class Program
     {
-        input = Console.ReadLine();
-        if (int.TryParse(input, out player) && player >= 1 && player <= 9)
+        static void Main(string[] args)
         {
-            break;
-        }
-        else
-        {
-            Console.WriteLine("Invalid input. Please enter a number between 1 and 9.");
-        }
-    }
+            // welcoming
+            Console.WriteLine("Welcome to Tic-Tac-Toe!");
 
+            // setting up the array board
+            string[] board = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-    player = int.Parse(Console.ReadLine());
+            // calling the new GameTools.cs page on the other side
+            GameTools game = new GameTools();
+            string currentPlayer = "X";
+            bool gameWon = false;
 
-    board[player - 1] = 'X';
-
-    // Check for a winner
-    if (gt.checkForWinner(board) == true)
-    {
-        Console.WriteLine("Player X wins!");
-        gt.displayBoard(board);
-        gameOver = true;
-    }
-    else
-    {
-        gt.displayBoard(board);
-
-        Console.WriteLine("Player O, please enter the number of an open space between 1 and 9: ");
-        // player = int.Parse(Console.ReadLine());
-
-        while (true)
-        {
-            input = Console.ReadLine();
-            if (int.TryParse(input, out player) && player >= 1 && player <= 9)
+            while (!gameWon && !game.IsBoardFull(board))
             {
-                break;
-            }
-            else
-            {
-                Console.WriteLine("Invalid input. Please enter a number between 1 and 9.");
-            }
-        }
+                // Print the board
+                game.PrintBoard(board);
 
-        board[player - 1] = 'O';
-        // Check for a winner
-        if (gt.checkForWinner(board) == true)
-        {
-            Console.WriteLine("Player O wins!");
-            gt.displayBoard(board);
-            gameOver = true;
+                // Get player's choice
+                Console.WriteLine($"Player {currentPlayer}, choose a position (1-9):");
+                string input = Console.ReadLine();
+
+                // Validate and update the board
+                if (int.TryParse(input, out int position) && position >= 1 && position <= 9 &&
+                    board[position - 1] == input)
+                {
+                    board[position - 1] = currentPlayer;
+
+                    // check if there is a winner
+                    if (game.CheckWinner(board, currentPlayer))
+                    {
+                        gameWon = true;
+                        game.PrintBoard(board);
+                        Console.WriteLine($"Player {currentPlayer} wins!");
+                    }
+                    else
+                    {
+                        // switch player
+                        currentPlayer = currentPlayer == "X" ? "O" : "X";
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid move. Please try again.");
+                }
+            }
+            // if neither won and the board was full
+            if (!gameWon)
+            {
+                game.PrintBoard(board);
+                Console.WriteLine("It's a draw!");
+            }
         }
     }
 }
-
-Console.WriteLine("Thank you for playing!");
