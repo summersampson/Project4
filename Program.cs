99ce9b78008d948b1fbea9d8e4aeda327bc07822
trey1144 <trey1144@hotmail.com>
2025-01-27 08:51:59 -0700

    Work in the Driver class, Tic-Tac-Toe logic :)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Welcome the user
+Console.WriteLine("Welcome to Tic-Tac-Toe!");
+
+using Project4;
+
+gameTools gt = new gameTools();
+
+bool gameOver = false;
+
+char[] board = new char[9];
+
+int player = 0;
+
+while (gameOver == false) 
+{
+    // Display the board
+    gt.displayBoard(board);
+
+    Console.WriteLine("Player X, please enter the number of an open space between 1 and 9: ");
+
+    player = int.Parse(Console.ReadLine());
+
+    board[player - 1] = 'X';
+
+    // Check for a winner
+    if (gt.checkForWinner(board) == true)
+    {
+        Console.WriteLine("Player X wins!");
+        gt.displayBoard(board);
+        gameOver = true;
+    }
+    else
+    {
+        gt.displayBoard(board);
+
+        Console.WriteLine("Player O, please enter the number of an open space between 1 and 9: ");
+        player = int.Parse(Console.ReadLine());
+        board[player - 1] = 'O';
+        // Check for a winner
+        if (gt.checkForWinner(board) == true)
+        {
+            Console.WriteLine("Player O wins!");
+            gt.displayBoard(board);
+            gameOver = true;
+        }
+    }
+}
+
+Console.WriteLine("Thank you for playing!");
